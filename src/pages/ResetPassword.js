261c3ebc8cc4397d8ff67178c6e0f4311146267e
JennieKieu <kieuduong2k2@gmail.com>
2024-04-03 12:11:02 +0700

    done connect forgotpass and reset pass

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,69 +1,81 @@
-import  React, {useState}  from "react";
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import styled from "styled-components";
 import route from "../configs/route";
-import axiosClient from "../api/axiosClient";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const ContainerStyled = styled(Container)`
-    display: flex;
-    justify-content:center;
-`
+  display: flex;
+  justify-content: center;
+`;
 
 const FormGroupStyled = styled(Form.Group)`
-    margin-bottom: 2rem;
-    text-align: center;
-`
+  margin-bottom: 2rem;
+  text-align: center;
+`;
 
-const ResetPassword =() => {
-    const [email, setEmail] = useState("");
-   
+const ResetPassword = () => {
+  const navigate = useNavigate();
+  const { id, token } = useParams();
+  const [password, setPassword] = useState();
+  axios.defaults.withCredentials = true;
 
-    const handleSubmit = (event) => {
-        event.preventDefault(); 
-        console.log({email}); 
-        alert("Vui lòng kiểm tra email của bạn");
-        axiosClient.post(`/users/forgot-password`, {
-          email,
-        });
-       
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ token, password });
+    alert("Đổi mật khẩu thành công");
+    axios
+      .post(`http://localhost:3000/api/users/reset-password/${id}/${token}`, {
+        password,
+      })
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          navigate(route.home);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
-      
+  return (
+    <ContainerStyled fluid="md">
+      <Form onSubmit={handleSubmit} style={{ width: "40%" }}>
+        <FormGroupStyled>
+          <h3>Đặt lại mật khẩu</h3>
+        </FormGroupStyled>
 
-    return (
-      <ContainerStyled fluid="md">
-        <Form onSubmit={handleSubmit}>
-          <FormGroupStyled>
-            <h4>Khôi phục mật khẩu Meme để kết nối với ứng dụng</h4>
-          </FormGroupStyled>
-          <FormGroupStyled>
-            <h1 style={{ color: "var(--primary)" }}>MEME</h1>
-          </FormGroupStyled>
+        <FormGroupStyled>
+          <Form.Control
+          autoComplete="off"
+            size="lg"
+            type="password"
+            name="password"
+            placeholder="Nhập mật khẩu mới"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormGroupStyled>
 
-          <FormGroupStyled controlId="resetPasswordPhone">
-            <Form.Control
-              size="lg"
-              type="text"
-              name="resetPasswordPhone"
-              placeholder="Nhập email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormGroupStyled>
+        {/* <FormGroupStyled>
+          <Form.Control
+            size="lg"
+            type="text"
+            name="newPasswordConfirm"
+            placeholder="Nhập lại mật khẩu mới"
+            // value={formData.resetPasswordPhone}
+            // onChange={handleChange}
+          />
+        </FormGroupStyled> */}
 
-          <FormGroupStyled>
-            <Row>
-              <Col>
-                <Button variant="primary" type="submit">
-                  Tiếp theo
-                </Button>
-              </Col>
-            </Row>
-          </FormGroupStyled>
-        </Form>
-      </ContainerStyled>
-    );
-}
+        <FormGroupStyled>
+          <Button variant="primary" type="submit">
+            Xác nhận
+          </Button>
+        </FormGroupStyled>
+      </Form>
+    </ContainerStyled>
+  );
+};
 
 export default ResetPassword;
