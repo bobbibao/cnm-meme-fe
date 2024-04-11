6f77866f12d57146bf0c9d4c0ce213e7030c2c8d
bobbibao <lehoangbao1125@gmail.com>
2024-04-11 18:57:38 +0700

    Add routes and declare page on App

--- src/configs/route.js
+++ src/configs/route.js
@@ -1,11 +1,16 @@
 const route = {
-  home: "/",
-  register: "/register",
-  registerConfirm: "/register-confirm/",
+	home: '/',
+	register:'/register',
+	registerConfirm: "/register-confirm/",
   forgotPassword: "/forgot-password",
-  resetPasswordConfirm: "/reset-password-confirm/",
-  resetPassword: "/reset-password/:id/:token",
-  chat: "/chat"
+	resetPasswordConfirm:'/reset-password-confirm',
+	resetPassword: "/reset-password/:id/:token",
+	sendOtp:"/send-otp",
+	chat:'/chat',
+	messages:'/messages/:id',
+	meetingView:'/meeting/:meetingId',
+	callList: "/call-list",
+	friendRequest: "/friend-request",
 };
 
 export default route;
