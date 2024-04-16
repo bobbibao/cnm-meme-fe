5bd7b6f12e49a639da77c71cb12cc15ceefdd2b1
BaoooDev <baootranvan@gmail.com>
2024-04-16 18:18:59 +0700

    createGroup w name n members

--- src/components/chat/FriendItem.js
+++ src/components/chat/FriendItem.js
@@ -8,26 +8,37 @@ const FriendItem =({friend, index, onCheck}) => {
     const handleItemClick = () => {
         const newCheckedState = !isChecked;
         setIsChecked(newCheckedState);
-        onCheck(index, newCheckedState); // Call the callback function with the index and checked state
+        onCheck(friend._id, newCheckedState); // Call the callback function with the index and checked state
       };
     return (
-        <StyledListGroupItem
+      <StyledListGroupItem
         key={index}
         action
         onClick={handleItemClick}
-        className="d-flex justify-content-between align-items-start border-0 ">
-            <Form.Check className='d-flex px-3 py-2'>
-            <Form.Check.Input style={{marginTop: "7px"}}
-                type='radio' isValid  checked={isChecked} />
-            <Form.Check.Label className='text-muted'>
-                <div className="d-flex align-items-center">
-                    <img src={friend.avatar} alt={friend.userName} width="30" height="30" className="mx-3" />
-                    <span>{friend.userName}</span>
-                </div>
-            </Form.Check.Label>
-            </Form.Check>
-        </StyledListGroupItem>
-    )
+        className="d-flex justify-content-between align-items-start border-0 "
+      >
+        <Form.Check className="d-flex px-3 py-2">
+          <Form.Check.Input
+            style={{ marginTop: "7px" }}
+            type="radio"
+            isValid
+            checked={isChecked}
+          />
+          <Form.Check.Label className="text-muted">
+            <div className="d-flex align-items-center">
+              <img
+                src={friend.photoURL}
+                alt={friend.username}
+                width="30"
+                height="30"
+                className="mx-3"
+              />
+              <span>{friend.username}</span>
+            </div>
+          </Form.Check.Label>
+        </Form.Check>
+      </StyledListGroupItem>
+    );
 }
 
 const StyledListGroupItem = styled(ListGroup.Item)`
